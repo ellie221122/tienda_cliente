090ee668ed33b8317d901363dd67d00db437c27e
ellie221122 <hobielola05@gmail.com>
2025-03-27 11:04:08 -0500

    menu
    hice menu

---
+++ practicas/tienda_cliente.py
@@ -0,0 +1,12 @@
+def hacer_menu():
+    print("1.registar cliente")
+    print("2.eliminar cliente")
+    print("0.salir del sistema")
+    print("...selecione una opcion...")
+    
+    opcion= int(input())
+    return opcion
+ 
+ #zona codigo 
+ 
+ aux_opcion = hacer_menu()
